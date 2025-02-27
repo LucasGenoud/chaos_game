67339d0fd86afccf3083eaa7d0ff31c06d1705e3
LucasGenoud <lucas.genoud@hes-so.ch>
2025-02-27 20:05:49 +0100

    chore: updated project github pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/sierpinski_triangle/",
+  base: "/chaos_game_fractal/",
   plugins: [
     vue(),
     vueDevTools(),
